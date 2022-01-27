88820dd46299e37367ef64ec26c31ea7d2569e43
Jurita <97666606+Jurita@users.noreply.github.com>
2022-01-27 15:05:29 +0200

    māa

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-alert(1);
+
 const vardi=['DŽeina Arenda','Jurita Stikane ','Signe Līce','Linda Atvare','Jurita Stikāne']
-
 const balvas=['Iphone 13','Dators','Māja','Dzīvoklis','Mašīna']
-
 const naudaKopa=1000000;//kopeja summa
-
 let uzvaretajuSkaits=5;
-
+let rindas = document.querySelector('.rindas');
+rindas.innerHTML= ' ';
+for(let i = 0; i < uzvaretajuSkaits; i++ ) {
 let rand= Math.random()*5;
-
 rand= Math.floor(rand);//noapalo uz leju
-
-console.log(vardi[rand]);//izvada konsole
+let uzvaretajs = vardi[rand]; 
+rindas.innerHTML += `
+<tr>
+<td>${i+1}</td>
+<td>${uzvaretajs}</td>
+</tr>`
+}
